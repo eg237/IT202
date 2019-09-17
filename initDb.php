fb95f2c2f574a887eaa47ff3cb9ded53d47f9c03
Eduardo Gomez <eg237@afsconnect1.njit.edu>
2019-09-17 11:14:34 -0400

    Database

--- initDb.php
+++ initDb.php
@@ -1,5 +1,18 @@
 <?
-require('conf.php');
+ini_set('display_errors',1);
+init_set('display_startup_errors',1);
+error_reporting(E_ALL);
 
+require('conf.php');
 echo $host;
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+
+try{
+	$db = new PDO($conn_string, $username, $password);
+	echo "Connected";
+}
+catch(Exception $e){
+	echo $e->getMessage();
+	exit("Something went wrong");
+}
 ?>
