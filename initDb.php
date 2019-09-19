c2024beda41b7721dbaaf4357d086cbabf17d423
eg237 <eg237@njit.edu>
2019-09-19 10:42:54 -0400

    Updated files

--- initDb.php
+++ initDb.php
@@ -3,14 +3,14 @@ ini_set('display_errors',1);
 init_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
-require("conf.php");
+require('conf.php');
 echo $host;
 $conn_string = "mysq2:host=$host;dbname=$database;charset=utf8mb4";
 
 try{
 	$db = new PDO($conn_string, $username, $password);
 	echo "Connected";
-	$query = "create table if not exist 'TestUser'(
+	$query = "create table if not exist `TestUser`(
 		`id` int auto_increment not null,
 		`username` varchar(30) not null unique,
 		`pin` int default 0,
@@ -18,7 +18,7 @@ try{
 		) CHARACTER SET utf8 COLLATE utf8_general_ci";
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 	$stmt = $db->prepare($query);
-	print_r($stmt->errorInfor());
+	print_r($stmt->errorInfo());
 	$r = $stmt->execute();
 	echo "<br>" . ($r>0?"Created table or already exists":"Failed to create table") . "<br>";
 	unset($r);
