5da41b59a6c7273fd609fd0a68ee401fa5485a83
eg237 <eg237@njit.edu>
2019-09-19 09:59:37 -0400

    Test

--- initDb.php
+++ initDb.php
@@ -1,10 +1,11 @@
-<?
+<?php
 ini_set('display_errors',1);
 init_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
 require('conf.php');
 echo $host;
+
 $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 
 try{
