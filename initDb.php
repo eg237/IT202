45ae1636e1cc565d51132dd62ba842b6e50903d5
eg237 <eg237@njit.edu>
2019-09-19 10:28:56 -0400

    Updated files

--- initDb.php
+++ initDb.php
@@ -16,9 +16,25 @@ try{
 		'pin' int default 0,
 		PRIMARY KEY ('id')
 		) CHARACTER SET utf8 COLLATE utf8_general_ci";
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 	$stmt = $db->prepare($query);
+	print_r($stmt->errorInfor());
 	$r = $stmt->execute();
-	echo "<br>" . $r . "<br>";
+	echo "<br>" . ($r>0?"Created table or already exists":"Failed to create table") . "<br>";
+	unset($r);
+
+	$insert_query = "INSERT INTO 'TestUsers' ('username', 'pin') 
+		VALUES (:username,:pin)";
+	$stmt = $db->prepare($insert_query);
+	$newUser = "Billy"
+	$newPin = "1234"
+	$r = $stmt->execute(array(":username"=> $newUser, ":pin" =$newPin));
+	
+	print_r($stmt->errorInfo());
+	
+	
+	echo "<br>" . ($r>0?"Insert successful":"Insert failed") . "<br>";
+	
 }
 catch(Exception $e){
 	echo $e->getMessage();
