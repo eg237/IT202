f3d969af4ad54a8b656bca3cef4911c0e57e8272
eg237 <eg237@njit.edu>
2019-09-24 11:17:26 -0400

    Re

--- initDb2.php
+++ initDb2.php
@@ -3,8 +3,8 @@
 ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-//pull in config.php so we can access the variables from it
-require('config.php');
+//pull in conf.php so we can access the variables from it
+require('conf.php');
 //echo "Loaded Host: " . $host;
 $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 try{
