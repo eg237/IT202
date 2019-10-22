a0f317844e494e4a0a77e49dd7531dbc932195cb
eg237 <eg237@njit.edu>
2019-10-22 14:01:41 -0400

    registration

--- Login/Registration.php
+++ Login/Registration.php
@@ -31,16 +31,12 @@
                         <hr class="mb-3">
                         <label for="firstname"><b>First Name</b></label>
                         <input type="text" name="firstname" required>
-                        <br>
                         <label for="lastName"><b>Last Name</b></label>
                         <input type="text" name="lastName" required>
-                        <br>
                         <label for="email"><b>Email Address</b></label>
                         <input type="email" name="email" required>
-                        <br>
                         <label for="password"><b>Password</b></label>
                         <input type="password" name="password" required>
-                        <br>
                         <hr class="mb-3">
                         <input class="btn btn-primary" type="submit" name="create" value="Sign Up" required>
                     </div>
