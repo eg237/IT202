fcf3e6a188be3e4d884342ed012a44672604d2bc
eg237 <eg237@njit.edu>
2019-10-22 12:38:26 -0400

    registration

--- Login/Registration.php
+++ Login/Registration.php
@@ -19,21 +19,25 @@
     <div>
         <form action="registration.php" method="post">
             <div class="container">
-                <h1>Registration</h1>
-                <p>Fill up the form with correct Values.</p>
-                <label for="firstname"><b>First Name</b></label>
-                <input type="text" name="firstname" required>
-                <br>
-                <label for="lastName"><b>Last Name</b></label>
-                <input type="text" name="lastName" required>
-                <br>
-                <label for="email"><b>Email Address</b></label>
-                <input type="email" name="email" required>
-                <br>
-                <label for="password"><b>Password</b></label>
-                <input type="password" name="password" required>
-                <br>
-                <input type="submit" name="create" value="Sign Up" required>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h1>Registration</h1>
+                        <p>Fill up the form with correct Values.</p>
+                        <label for="firstname"><b>First Name</b></label>
+                        <input type="text" name="firstname" required>
+                        <br>
+                        <label for="lastName"><b>Last Name</b></label>
+                        <input type="text" name="lastName" required>
+                        <br>
+                        <label for="email"><b>Email Address</b></label>
+                        <input type="email" name="email" required>
+                        <br>
+                        <label for="password"><b>Password</b></label>
+                        <input type="password" name="password" required>
+                        <br>
+                        <input type="submit" name="create" value="Sign Up" required>
+                    </div>
+                </div>
             </div>
     </div>
 </body>
