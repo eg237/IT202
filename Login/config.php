b50e9f26e2a314e8cfd8e09690cece8fb2c150aa
eg237 <eg237@njit.edu>
2019-10-22 17:38:54 -0400

    registration

--- Login/config.php
+++ Login/config.php
@@ -6,3 +6,4 @@ $db_name = "UserAccounts";
 
 $db = new PDO('mysql:host=sql.njit.edu;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
