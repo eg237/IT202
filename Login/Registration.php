4619df2a1fb3226399533d20986116c39e6a7d8b
eg237 <eg237@njit.edu>
2019-10-22 17:25:34 -0400

    registration

--- Login/Registration.php
+++ Login/Registration.php
@@ -4,9 +4,9 @@ require_once('config.php');
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- <meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>User Registration | PHP</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
