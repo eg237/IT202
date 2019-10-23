b36ee9866f18ce6d841f9bcfb244147aefd4e7c9
eg237 <eg237@njit.edu>
2019-10-22 21:53:22 -0400

    test

--- Login/config.php
+++ Login/config.php
@@ -5,19 +5,19 @@ $db_user = "eg237";
 $db_pass = "LL9acXZx";
 $db_name = "eg237";
 
-//$conn =mysqli_connect($servername, $db_user, $db_pass, $db_name);
-$conn = new mysqli_connect($servername, $db_user, $db_pass, $db_name);
+$conn =mysqli_connect($servername, $db_user, $db_pass, $db_name);
+// $conn = new mysqli_connect($servername, $db_user, $db_pass, $db_name);
 if(!$conn){
     die("Connection Failed: ".mysqli_connect_error());
     }
-//else{
-//     $stmt = $conn->prepare("Insert into Registration(firstname, lastname, email, password) value(?,?,?,?)");
-//     $stmt->bind_param("ssss", $firstname, $lastname, $email, $password);
-//     $stmt->execute();
-//     echo "Successful Registration";
-//     $stmt->close();
-//     $conn->close();
-// }
+else{
+    $stmt = $conn->prepare("Insert into Registration(firstname, lastname, email, password) value(?,?,?,?)");
+    $stmt->bind_param("ssss", $firstname, $lastname, $email, $password);
+    $stmt->execute();
+    echo "Successful Registration";
+    $stmt->close();
+    $conn->close();
+}
 //$db = new PDO('mysql:host=sql1.njit.edu;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
 //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
