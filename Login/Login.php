48b059179b6ed2dc38d34fc90a02c1de077d10a1
eg237 <eg237@njit.edu>
2019-10-26 08:50:04 -0400

    Login

--- Login/Login.php
+++ Login/Login.php
@@ -129,7 +129,7 @@ error_reporting(E_ALL);
                 </div>
             </div>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
     <script type="text/javascript">
         $(function(){
@@ -140,6 +140,6 @@ error_reporting(E_ALL);
                 'type': 'success',
             })
         })
-    </script>
+    </script> -->
 </body>
 </html>
