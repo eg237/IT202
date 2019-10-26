310cab820643ff68bb7bc86f6e5793cf8c0fd869
eg237 <eg237@njit.edu>
2019-10-26 09:07:39 -0400

    Login

--- Login/Login.php
+++ Login/Login.php
@@ -82,7 +82,7 @@ error_reporting(E_ALL);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>User Registration | PHP</title>
+    <title>Login | PHP</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 <body>
@@ -102,7 +102,10 @@ error_reporting(E_ALL);
             //  }else{
             //      echo 'There were errors while saving the data ';
             //  }
-            echo "$username";
+            // echo "$username";
+            if(!$query){
+                die("invalid Query:" . mysql_error());
+            }
          }
         ?>
     </div>
