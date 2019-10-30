af43214f4d9368bac5bba3d914ce9120a7f835a9
Eddy Gomez <egomez@freshpet.com>
2019-10-30 09:15:59 -0400

    Login

--- Login/Login.php
+++ Login/Login.php
@@ -1,23 +1,25 @@
-<!-- // include_once('config.php');
-// ini_set('display_errors',1);
-// ini_set('display_startup_errors',1);
-// error_reporting(E_ALL);
+<?php
+include_once('config.php');
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
 
-// mysql_select_db($db_name);
+mysql_select_db($db_name);
 
-// if(isset([$_POST['create'])){
-//     $username = $_POST['username'];
-//     $password = $_POST['password'];
+if(isset([$_POST['create'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
     
-//     $sql="SELECT * FROM `UserAccounts` WHERE username='$username' AND pass='$password' LIMIT 1 ";
-//     if(mysql_num_rows($result)==1){
-//         echo "You have Successfully Logged in";
-//         exit();
-//     }else{
-//         echo "You have entered incorrect credentials";
-//     }
-// } -->
-<!-- <!DOCTYPE html>
+    $sql="SELECT * FROM `UserAccounts` WHERE username='$username' AND pass='$password' LIMIT 1 ";
+    if(mysql_num_rows($result)==1){
+         echo "You have Successfully Logged in";
+        exit();
+    }else{
+        echo "You have entered incorrect credentials";
+    }
+} 
+?>
+ <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -45,7 +47,7 @@
         </div>
     </div>
 </body>
-</html> -->
+</html> 
 
 <!-- <header>
     <nav>
@@ -69,75 +71,3 @@
         </form>
     </div>
 </header> -->
-<?php
-// require once not working
-include_once('config.php');
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(E_ALL);
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login | PHP</title>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-</head>
-<body>
-    <div>
-        <?php
-        //require('config.php');
-        if(isset($_POST['create'])){
-            $username   = $_POST['username'];
-            $password    = $_POST['password'];
-            
-            $query = mysqli_query("SELECT * FROM `UserAccounts` WHERE username='$username' AND pass='$password'");
-            
-            if(!$query){
-                die("invalid Query:" . mysql_error());
-            }
-         }
-        ?>
-    </div>
-
-    <div>
-        <form action="Login.php" method="post">
-            <div class="container">
-                
-                <div class="row">
-                    <div class="col-sm-3">
-                        <h1>Login</h1>
-                        <p>Enter Your Credentials</p>
-                        <hr class="mb-3">
-
-                        <label for="username"><b>Username</b></label>
-                        <input type="text" name="username" required>
-
-                        <br>
-                        <label for="password"><b>Password</b></label>
-                        <br>
-                        <input type="password" name="password" required>
-
-                        <hr class="mb-3">
-                        <input class="btn btn-primary" type="submit" name="create" value="Sign Up" required>
-                    </div>
-                </div>
-            </div>
-    </div>
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
-    <script type="text/javascript">
-        $(function(){
-            // alert("Hello");
-            Swal.fire({
-                'title':'Register',
-                'text':'Only use Credentials you will Remember',
-                'type': 'success',
-            })
-        })
-    </script> -->
-</body>
-</html>
-    <!--- https://www.php.net/manual/en/mysqli.select-db.php --->
