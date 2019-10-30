a7f2e7b7b69e424f44e3584b043550eb7fb26505
Eddy Gomez <egomez@freshpet.com>
2019-10-30 10:24:16 -0400

    Login

--- Login/Login.php
+++ Login/Login.php
@@ -14,7 +14,7 @@ if(isset([$_POST['create'])){
     }
     
     //$query="SELECT username, pass FROM `UserAccounts` WHERE username=? AND pass=? LIMIT 1";
-    $stmt = $db->prepare($query);
+    //$stmt = $db->prepare($query);
     // $stmt->bind_param("ss",$username,$password);
     // $stmt->execute();
     // $stmt->bind_result($username, $password);
