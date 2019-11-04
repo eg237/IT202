d8132b6d9138615961c76674d795bad5da5b1f01
eg237 <eg237@njit.edu>
2019-11-04 18:14:28 -0500

    index

--- Login/index.php
+++ Login/index.php
@@ -8,12 +8,20 @@ $password = $_POST['pass'];
  if(isset($_POST['create'])){
 
     $sql="SELECT * FROM 'UserAccounts' WHERE username='$username' and pass='$password'";
-    $result=mysqli_query($sql);
-    $row = mysqli_fetch_array($result);
-    if ($row['username'] == $username && $row['pass'] == $password){
-        echo "You are logged in";
-    }else{
-        echo 'Incorrect Info';
+    $result=$db->query($sql);
+    $row = $result->fetch_assoc();
+    // if ($row['username'] == $username && $row['pass'] == $password){
+    //     echo "You are logged in";
+    // }else{
+    //     echo 'Incorrect Info';
+    // }
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        }
+    } else {
+        echo "0 results";
     }
 
  }
