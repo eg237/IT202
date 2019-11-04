e91e501e0f9644a31c648e74f1c9919ba65fa2b4
eg237 <eg237@njit.edu>
2019-11-04 18:02:58 -0500

    index

--- Login/index.php
+++ Login/index.php
@@ -9,17 +9,7 @@ $password = $_POST['pass'];
 
     $sql="SELECT * FROM 'UserAccounts' WHERE username='$username' and pass='$password'";
     $result=mysqli_query($sql);
-    echo $sql;
-    // if($row = mysqli_fetch_assoc($result)){
-    //     $db_password = $row['pass']
-
-    //     if(md5($password)== $db_password){
-    //         echo 'You are In';
-    //     }
-    // }
-    // else{
-    //     echo 'Please check your Query';
-    // }
+    echo $result;
 
  }
 
