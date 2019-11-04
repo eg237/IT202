f205d840e2ca562326b4fd3b68b91b6841c7da04
eg237 <eg237@njit.edu>
2019-11-04 17:24:49 -0500

    Index

--- Login/index.php
+++ Login/index.php
@@ -1,16 +1,20 @@
 <?php
-require_once('config.php');
-if(isset($_POST['create'])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+include_once('config.php');
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
 
-    if(empty($username)|| empty($password)){
-        echo 'Please Fill in the blanks'
-    }
-    // else{
-    //     $query = "select * from 'UserAccounts' where username='$username' ";
-    //     $result = mysqli_query($db,$query);
-    // }
-}
+// if(isset($_POST['create'])){
+//     $username = $_POST['username'];
+//     $password = $_POST['password'];
+
+//     if(empty($username)|| empty($password)){
+//         echo 'Please Fill in the blanks'
+//     }
+//     // else{
+//     //     $query = "select * from 'UserAccounts' where username='$username' ";
+//     //     $result = mysqli_query($db,$query);
+//     // }
+// }
 
 ?>
