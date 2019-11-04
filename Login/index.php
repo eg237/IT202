73f2313d3861229cd0154fc0141686f8539e12fc
Eddy Gomez <egomez@freshpet.com>
2019-11-04 09:46:49 -0500

    Index

--- Login/index.php
+++ Login/index.php
@@ -1,21 +1,22 @@
-<?php 
+<?php
+    include_once('config.php')
     $username = $_POST['username']
     $password = $_POST['password']
 
     // $username = stripcslashes($username);
     // $password = stripcslashes($password);
-$servername="sql2.njit.edu";
-$db_user = "eg237";
-$db_pass = "LL9acXZx";
-$db_name = "eg237";
+// $servername="sql2.njit.edu";
+// $db_user = "eg237";
+// $db_pass = "LL9acXZx";
+// $db_name = "eg237";
 
-$db=mysqli_connect($servername, $db_user, $db_pass, $db_name);
+// $db=mysqli_connect($servername, $db_user, $db_pass, $db_name);
 
-if($db){
-    echo "Connected";
-}else{
-    echo "Not Connected";
-}
+// if($db){
+//     echo "Connected";
+// }else{
+//     echo "Not Connected";
+// }
 
 $result = mysqli_query("SELECT * FROM `UserAccounts` WHERE username = '$username' and pass = '$password'") 
     or die("Failed to query Database " . mysqli_error());
