8c8afc303c2d73faf67ac4559c8ebe4a7b9fdcbf
eg237 <eg237@njit.edu>
2019-11-04 18:16:09 -0500

    index

--- Login/index.php
+++ Login/index.php
@@ -17,7 +17,7 @@ $password = $_POST['pass'];
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
-            echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+            echo 'Good';
         }
     } else {
         echo "0 results";
