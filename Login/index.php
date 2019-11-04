93232f1d404578470322d6cb26cea1d1b9d44548
Eddy Gomez <egomez@freshpet.com>
2019-11-04 09:10:32 -0500

    Index

--- Login/index.php
+++ Login/index.php
@@ -1,12 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index | PHP</title>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-</head>
-<body>
-    <h1>Test</h1>
-</body>
+<?php 
+    $username = $_POST['username']
+    $password = $_POST['password']
+
+    // $username = stripcslashes($username);
+    // $password = stripcslashes($password);
+$servername="sql2.njit.edu";
+$db_user = "eg237";
+$db_pass = "LL9acXZx";
+$db_name = "eg237";
+
+$db=mysqli_connect($servername, $db_user, $db_pass, $db_name);
+
+if($db){
+    echo "Connected";
+}else{
+    echo "Not Connected";
+}
+
+$result = mysqli_query("SELECT * FROM")
+
+?>
