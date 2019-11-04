7fa4acbcdeec1d03bf69b262b674bf456b88bd95
eg237 <eg237@njit.edu>
2019-11-04 18:24:44 -0500

    index

--- Login/index.php
+++ Login/index.php
@@ -7,34 +7,13 @@ $password = $_POST['pass'];
 
  if(isset($_POST['create'])){
 
-    $sql="SELECT * FROM 'UserAccounts' ";
-    $result = mysqli_query($db,$sql);
-    if(mysqli_num_rows($result) > 0){
-        echo "<table>";
-            echo "<tr>";
-                echo "<th>id</th>";
-                echo "<th>first_name</th>";
-                echo "<th>last_name</th>";
-                echo "<th>email</th>";
-            echo "</tr>";
-        while($row = mysqli_fetch_array($result)){
-            echo "<tr>";
-                echo "<td>" . $row['id'] . "</td>";
-                echo "<td>" . $row['first_name'] . "</td>";
-                echo "<td>" . $row['last_name'] . "</td>";
-                echo "<td>" . $row['email'] . "</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-        // Free result set
-        mysqli_free_result($result);
-    } else{
-        echo "No records matching your query were found.";
-    }
-} else{
-    echo "ERROR: Could not able to execute $sql. " . 
+    $sql="SELECT * FROM 'UserAccounts' WHERE username='$username' and pass='$password'";
+    $result=$db->query($sql);
 
-}
-}
+    if($result->num_rows > 0){
+        echo 'Good';
+    }
+
+ }
 
 ?>
