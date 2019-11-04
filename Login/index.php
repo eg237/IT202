cd6498ca3ab9066eb3ac253f5bfc204d54d9beba
eg237 <eg237@njit.edu>
2019-11-04 17:30:45 -0500

    index

--- Login/index.php
+++ Login/index.php
@@ -1,21 +1,17 @@
 <?php
 include_once('config.php');
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(E_ALL);
 
  if(isset($_POST['create'])){
-     echo 'Test';
-//     $username = $_POST['username'];
-//     $password = $_POST['password'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-//     if(empty($username)|| empty($password)){
-//         echo 'Please Fill in the blanks'
-//     }
-//     // else{
-//     //     $query = "select * from 'UserAccounts' where username='$username' ";
-//     //     $result = mysqli_query($db,$query);
-//     // }
+    if(empty($username)|| empty($password)){
+        echo 'Please Fill in the blanks'
+    }
+    // else{
+    //     $query = "select * from 'UserAccounts' where username='$username' ";
+    //     $result = mysqli_query($db,$query);
+    // }
  }
 
 ?>
