658b940b655ef68681b57635082a00af6bf91b44
Eddy Gomez <egomez@freshpet.com>
2019-11-04 09:47:56 -0500

    Index

--- Login/index.php
+++ Login/index.php
@@ -5,19 +5,6 @@
 
     // $username = stripcslashes($username);
     // $password = stripcslashes($password);
-// $servername="sql2.njit.edu";
-// $db_user = "eg237";
-// $db_pass = "LL9acXZx";
-// $db_name = "eg237";
-
-// $db=mysqli_connect($servername, $db_user, $db_pass, $db_name);
-
-// if($db){
-//     echo "Connected";
-// }else{
-//     echo "Not Connected";
-// }
-
 $result = mysqli_query("SELECT * FROM `UserAccounts` WHERE username = '$username' and pass = '$password'") 
     or die("Failed to query Database " . mysqli_error());
 $row = mysqli_fetch_array($result);
