002755684b362cdc30b473edf8cf4b6c4f40a315
eg237 <eg237@njit.edu>
2019-11-05 10:15:44 -0500

    Index

--- Login/index.php
+++ Login/index.php
@@ -2,20 +2,45 @@
 include_once('config.php');
 
 
-$username = $_POST['user'];
-$password = $_POST['pass'];
+// $username = $_POST['user'];
+// $password = $_POST['pass'];
 
- if(isset($_POST['create'])){
+//  if(isset($_POST['create'])){
 
-    $sql="SELECT * FROM 'UserAccounts' WHERE username='$username' and pass='$password'";
-    $result=mysqli_query($sql);
-    $row = mysqli_fetch_array($result);
-    if ($row['username'] == $username && $row['pass'] == $password){
-        echo "You are logged in";
-    }else{
-        echo 'Incorrect Info';
+//     $sql="SELECT * FROM 'UserAccounts' WHERE username='$username' and pass='$password'";
+//     $result=mysqli_query($sql);
+//     $row = mysqli_fetch_array($result);
+//     if ($row['username'] == $username && $row['pass'] == $password){
+//         echo "You are logged in";
+//     }else{
+//         echo 'Incorrect Info';
+//     }
+
+//  }
+
+if(isset($_POST['user']) && isset($_POST['pass'])){
+    $username = $_POST['user'];
+    $password = $_POST['pass'];
+
+    $query="select username , pass from `UserAccounts`";
+    $query += "where username = :user";
+    $stmt = $db->prepare($query);
+    $stmt->execute(array(":user" =>$username));
+    $result = $stmt->fetch(sqli::FETCH_ASSOC);
+    if($results && count($results) > 0){
+        //$hash = password_hash($pass, PASSWORD_BCRYPT);
+        if(password_verify($pass, $results['password'])){
+            echo "Welcome, " . $results["username"];
+            echo "[" . $results["username"] . "]";
+            //header("Location: samplelandingpage.html");
+        }
+        else{
+            echo "Invalid password";
+        }
     }
-
- }
+    else{
+            echo "Invalid username";
+    }
+}
 
 ?>
