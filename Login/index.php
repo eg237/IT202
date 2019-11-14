17680a59dd6c4f2b33391ae07cc9dcfcda98ea36
eg237 <eg237@njit.edu>
2019-11-14 10:30:11 -0500

    index

--- Login/index.php
+++ Login/index.php
@@ -1,5 +1,6 @@
 <?php
 include_once('config.php');
+session_start();
 
 
 // $username = $_POST['user'];
@@ -18,29 +19,55 @@ include_once('config.php');
 
 //  }
 
-if(isset($_POST['user']) && isset($_POST['pass'])){
-    $username = $_POST['user'];
-    $password = $_POST['pass'];
+// if(isset($_POST['user']) && isset($_POST['pass'])){
+//     $username = $_POST['user'];
+//     $password = $_POST['pass'];
 
-    $query="select username , pass from `UserAccounts`";
-    $query += "where username = :user LIMIT 1";
-    $stmt = $db->prepare($query);
-    $stmt->execute(array(":user" =>$username));
-    $result = $stmt->fetch(sqli::FETCH_ASSOC);
-    if($results && count($results) > 0){
-        // $hash = password_hash($pass, PASSWORD_BCRYPT);
-        // if(password_verify($password, $results['pass'])){
-            echo "Welcome, " . $results["user"];
-            echo "[" . $results["user"] . "]";
-            header("Location: https://web.njit.edu/~eg237/IT202/Login/LandingPage.html");
-        }
-        else{
-            echo "Invalid password";
-        }
+//     $query="select username , pass from `UserAccounts`";
+//     $query += "where username = :user LIMIT 1";
+//     $stmt = $db->prepare($query);
+//     $stmt->execute(array(":user" =>$username));
+//     $result = $stmt->fetch(sqli::FETCH_ASSOC);
+//     if($results && count($results) > 0){
+//         // $hash = password_hash($pass, PASSWORD_BCRYPT);
+//          if(password_verify($password, $results['pass'])){
+//             echo "Welcome, " . $results["user"];
+//             echo "[" . $results["user"] . "]";
+//             header("Location: LandingPage.html");
+//         }
+//         else{
+//             echo "Invalid password";
+//         }
+//     }
+//     else{
+//             echo "Invalid username";
+//     }
+// }
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    // username and password sent from form 
+    
+    $myusername = mysqli_real_escape_string($db,$_POST['username']);
+    $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+    
+    $sql = "SELECT id FROM `UserAccounts` WHERE username = '$myusername' and passcode = '$mypassword'";
+    $result = mysqli_query($db,$sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    $active = $row['active'];
+    
+    $count = mysqli_num_rows($result);
+    
+    // If result matched $myusername and $mypassword, table row must be 1 row
+      
+    if($count == 1) {
+       session_register("myusername");
+       $_SESSION['login_user'] = $myusername;
+       
+       header("location: LandingPage.php");
+    }else {
+       echo "Your Login Name or Password is invalid";
     }
-    else{
-            echo "Invalid username";
-    }
-}
+ }
+
 
 ?>
