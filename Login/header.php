57e22b3a889d2e4cce99364f639a49832032d087
eg237 <eg237@njit.edu>
2019-11-26 10:28:21 -0500

    Login

--- Login/header.php
+++ Login/header.php
@@ -4,25 +4,28 @@ require 'config.php'
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset = "utf-8">
-        <meta name = "description" content="This is an example of a meta description.">
-        <meta name=viewport content="width=device-width, initial-scale=1">
-        <title></title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="This is an example of a meta description. This will often show up in search results.">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
 
-        <header>
-            <nav class = "nav-header-main">
-                <a href="#"></a>
-                <ul>
-                    <li><a href="index.php">Home</li>
-                    <li><a href="#">Portfolio</li>
-                    <li><a href="#">About me</li>
-                    <li><a href="#">Contact</li>
-                </ul>
-                <div class="header-login">
+    <header>
+      <nav class="nav-header-main">
+        <a class="header-logo" href="index.php">
+          <img src="img/logo.png" alt="mmtuts logo">
+        </a>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">About me</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <div class="header-login">
                     <form action="Login.php" method="post">
                         <button type="submit" name="login-submit">Login</button>
                     </from>
