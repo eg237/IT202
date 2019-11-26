b7468a72c8bcbe4905cb1ae1b94533a1249cb104
eg237 <eg237@njit.edu>
2019-11-26 10:24:16 -0500

    Conf

---
+++ loginsystem/config.php
@@ -0,0 +1,30 @@
+ <?php
+
+$servername="sql2.njit.edu";
+$db_user = "eg237";
+$db_pass = "LL9acXZx";
+$db_name = "eg237";
+
+$db=mysqli_connect($servername, $db_user, $db_pass, $db_name);
+
+// if($db){
+//     echo "Connected";
+// }else{
+//     echo "Not Connected";
+// }
+
+//try{
+//$conn = new PDO('mysql:host=$servername;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
+//$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//echo "Connection Successful";
+// }
+// catch(PDOExeption $e){
+//     echo "Connection Failed: " . $e->getMessage(); 
+// }
+
+// $host="sql2.njit.edu";//your msql host [sql1, sql2, sql3]
+// $database="eg237";//ucid
+// $username="eg237";//ucid
+// $password="";//mysql password (not ucid password)
+
+?>
